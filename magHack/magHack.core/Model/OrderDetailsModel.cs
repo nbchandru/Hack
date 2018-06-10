5bf075a37cd2bdddcf856712594a6d55d39751c5
Balachandran Chandrasekar <BChandrasekar@simba.ad>
2018-06-10 18:14:18 +0530

    Signed-off-by: Balachandran Chandrasekar <BChandrasekar@simba.ad>

---
+++ magHack/magHack.core/Model/OrderDetailsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace magHack.core.Model
+{
+    [Serializable]
+    public class OrderDetailsModel
+    {
+        public LoginModel LoginDetails { get; set; }
+
+        public List<string> OrderList { get; set; }
+
+        public string CafeID { get; set; }
+
+        public string CafeMenueID { get; set; }
+    }
+}
